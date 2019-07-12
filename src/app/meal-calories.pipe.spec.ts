96a471b5f1ba2ab7d80a0b458dc4150dba1972ef
brian <brayomumo>
2019-07-12 08:05:11 +0300

    Add pipe to module

---
+++ src/app/meal-calories.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { MealCaloriesPipe } from './meal-calories.pipe';
+
+describe('MealCaloriesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new MealCaloriesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
